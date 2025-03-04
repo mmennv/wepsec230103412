430c3ced814937ab518b19e5e6d5299120cd5fac
mmennv <menna230103412@sut.edu.eg>
2025-03-04 23:36:51 +0200

    code

--- webSecSerivers/resources/views/layouts/navbar.blade.php
+++ webSecSerivers/resources/views/layouts/navbar.blade.php
@@ -1,18 +1,40 @@
 <nav class="navbar navbar-expand-sm bg-light">
-    <div class="container-fluid">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('home') }}">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('even') }}">Even Numbers</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('prime') }}">Prime Numbers</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('multable') }}">Multiplication Table</a>
-        </li>
-      </ul>
-    </div>
+  <div class="container-fluid">
+  <ul class="navbar-nav">
+  <li class="nav-item">
+   
+  
+  
+   
+  <a class="nav-link" href="./">Home</a>
+  </li>
+  <li class="nav-item">
+  <a class="nav-link" href="./even">Even Numbers</a>
+  </li>
+  <li class="nav-item">
+  <a class="nav-link" href="./prime">Prime Numbers</a>
+  </li>
+  <li class="nav-item">
+  <a class="nav-link" href="./multable">Multiplication Table</a>
+  </li>
+   <li class="nav-item">
+  <a class="nav-link" href="./bill">bill</a>
+  </li> 
+  <li class="nav-item">
+  <a class="nav-link" href="./transcript">transcript</a>
+  </li> 
+  <li class="nav-item">
+  <a class="nav-link" href="./products"> products list</a>
+  </li> 
+  <li class="nav-item">
+  <a class="nav-link" href="./users"> users</a>
+  </li> 
+  <li class="nav-item">
+  <a class="nav-link" href="./grades"> grades</a>
+  </li> 
+  <li class="nav-item">
+  <a class="nav-link" href="./exam"> exam</a>
+  </li> 
+  </ul>
+  </div>
   </nav>
--- webSecSerivers/routes/web.php
+++ webSecSerivers/routes/web.php
@@ -1,20 +1,97 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Web\ProductsController;
+use App\Http\Controllers\Web\UserController;
+use App\Http\Controllers\Web\GradeController;
+use App\Http\Controllers\Web\ExamController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
+
+Route::get('/text', function () {
+    return view('text'); //multable.blade.php
+});
 
 Route::get('/multable', function () {
-    return view('multable');
-})->name('multable');
+    return view('multable'); //multable.blade.php
+});
 
 Route::get('/even', function () {
-    return view('even_number');
-})->name('even');
-
+    return view('even'); //even.blade.php
+});
+    
 Route::get('/prime', function () {
-    return view('prime_number');
-})->name('prime');
+    return view('prime'); //prime.blade.php
+});
 
+Route::get('/bill', function () {
+    return view('bill'); //prime.blade.php
+});
+
+Route::get('/transcript', function () {
+    return view('transcript'); //prime.blade.php
+});
+
+Route::get('products', [ProductsController::class, 'list'])->name('products_list');
+Route::get('products/edit/{product?}', [ProductsController::class, 'edit'])->name('products_add');
+Route::post('products/save/{product?}', [ProductsController::class, 'save'])->name('products_save');
+Route::get('products/delete/{product}', [ProductsController::class, 'delete'])->name('products_delete');
+
+Route::get('/users', [UserController::class, 'list'])->name('users_list');
+Route::get('/users/add', [UserController::class, 'edit'])->name('users_add');
+Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users_edit');
+Route::post('/users/save/{user?}', [UserController::class, 'save'])->name('users_save');
+Route::get('/users/delete/{user}', [UserController::class, 'delete'])->name('users_delete');
+
+Route::get('/grades', [GradeController::class, 'list'])->name('grades_list');
+Route::get('/grades/add', [GradeController::class, 'edit'])->name('grades_add');
+Route::get('/grades/edit/{grade}', [GradeController::class, 'edit'])->name('grades_edit');
+Route::post('/grades/save/{grade?}', [GradeController::class, 'save'])->name('grades_save');
+Route::get('/grades/delete/{grade}', [GradeController::class, 'delete'])->name('grades_delete');
+
+Route::get('/exam', function () {
+    return view('exam.main');
+})->name('exam_main');
+
+Route::get('/exam/questions', [ExamController::class, 'manageQuestions'])->name('exam_manage_questions');
+Route::get('/exam/question/add', [ExamController::class, 'editQuestion'])->name('exam_add_question');
+Route::get('/exam/question/edit/{question}', [ExamController::class, 'editQuestion'])->name('exam_edit_question');
+Route::post('/exam/question/save/{question?}', [ExamController::class, 'saveQuestion'])->name('exam_save_question');
+Route::get('/exam/question/delete/{question}', [ExamController::class, 'deleteQuestion'])->name('exam_delete_question');
+
+Route::get('/exam/start', [ExamController::class, 'startExam'])->name('exam_start');
+Route::post('/exam/submit', [ExamController::class, 'submitExam'])->name('exam_submit');
+
+
+Route::get('/bill', function () {
+    $customer_name = 'mnmn';
+    $order_date = now()->toDateString();
+
+    $items = [
+        ['name' => 'tea',  'quantity' => 1, 'price' => 12.50],
+        ['name' => 'jam', 'quantity' => 3, 'price' => 32.00],
+        ['name' => 'banana',  'quantity' => 5, 'price' => 2.20],
+        ['name' => 'rice', 'quantity' => 2, 'price' => 15.75],
+    ];
+
+    $total_amount = array_sum(array_map(fn($item) => $item['quantity'] * $item['price'], $items));
+
+    return view('bill', compact('customer_name', 'order_date', 'items', 'total_amount'));
+});
+Route::get('/transcript', function () {
+    $student_name = 'mnmn';
+    $student_id = '3412';
+    $semester = 'Fall 2024';
+
+    $courses = [
+        ['course' => 'Mathematics', 'code' => 'MATH101', 'credits' => 3, 'grade' => 'A'],
+        ['course' => 'Physics', 'code' => 'PHYS102', 'credits' => 4, 'grade' => 'B+'],
+        ['course' => 'Computer Science', 'code' => 'CS103', 'credits' => 3, 'grade' => 'A-'],
+        ['course' => 'History', 'code' => 'HIST104', 'credits' => 2, 'grade' => 'B'],
+    ];
+
+    return view('transcript', compact('student_name', 'student_id', 'semester', 'courses'));
+});
