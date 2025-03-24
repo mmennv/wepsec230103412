2a069d0e1885961446456fde68fcdbbb79cf9185
mmennv <menna230103412@sut.edu.eg>
2025-03-24 12:09:25 +0200

    دددد

---
+++ WebSecService/resources/views/products/create.blade (1).php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@section('title', 'Prime Numbers')
+@section('content')
+    <h1>Create a Product</h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+
+
+        @endif
+    </div>
+    <form method="post" action="{{route('product.store')}}">
+        @csrf 
+        @method('post')
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="Qty" />
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="Price" />
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description" />
+        </div>
+        <div>
+            <input type="submit" value="Save a New Product" />
+        </div>
+    </form>
+    @endsection
